b253ce948472cab47a5189271c5520f52b80d79f
Charlie Juliana <charliejuliana@hunschool.org>
2025-10-09 12:55:59 -0400

    my room

---
+++ FX/HauntedMansion/src/ChambersOfDeath.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class ChambersOfDeath implements AppScene{
+
+}
+
