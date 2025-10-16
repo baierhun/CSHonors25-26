06c10b3720aad4f38956357eea2428b9409b5272
Charlie Juliana <charliejuliana@hunschool.org>
2025-10-16 11:17:05 -0400

    commiting files again

--- FX/HauntedMansion/src/ChambersOfDeath.java
+++ FX/HauntedMansion/src/ChambersOfDeath.java
@@ -22,46 +22,46 @@ public class ChambersOfDeath implements AppScene{
         description.setStyle("-fx-font-weight: bold; -fx-font-size: 12px;");
 
 
-        String secretText = "A hidden note appears: 'Beware the ghost!'";
-        String riddle = "I'm seen through glass, have no weight or clothes, \nand I only show up after your life is over—what am I?";
-        String answer = "ghost";
+        String secretText = "Millions of lice crawl on the bed and all over you.";
+        String behind = "You hear a whooshing sound behind you, but when you turn nothing is there.";
+        String answer = "lice";
         String red = "#991E06";
 
         Label secret = new Label(secretText);
         secret.setVisible(false);
-        secret.setStyle("-fx-font-weight: bold; -fx-font-size: 20;");
+        secret.setStyle("-fx-font-weight: bold; -fx-font-size: 15;");
 
         TextField input = new TextField();
-        input.setPromptText("Type a secret word...");
+        input.setPromptText("What's in the bed...(click check out bed for answer)");
         Button windows = new Button("Unboard the windows");
         // lambda function
         windows.setOnAction(e -> {
-            secret.setText(riddle);
+            secret.setText(behind);
             secret.setStyle(String.format("%s-fx-text-fill: %s;", secret.getStyle(), red));
             secret.setVisible(true);
         });
 
-        Ellipse ghost = new Ellipse(20.0, 40.0);
-        ghost.setFill(Color.WHITE);
-        ghost.setStroke(Color.BLACK);
-        ghost.setStrokeWidth(3);
-        ghost.setVisible(true);
+        Ellipse lice = new Ellipse(40.0, 20.0);
+        lice.setFill(Color.BLACK);
+        lice.setStroke(Color.BLACK);
+        lice.setStrokeWidth(3);
+        lice.setVisible(true);
 
         Circle leftEye = new Circle(0, 0, 5);
-        leftEye.setTranslateX(-5);
-        leftEye.setTranslateY(-25);
-        leftEye.setFill(Color.GREEN);
+        leftEye.setTranslateX(20);
+        leftEye.setTranslateY(-5);
+        leftEye.setFill(Color.RED);
 
         Circle rightEye = new Circle(0, 0, 5);
-        rightEye.setTranslateX(5);
-        rightEye.setTranslateY(-25);
-        rightEye.setFill(Color.GREEN);
+        rightEye.setTranslateX(20);
+        rightEye.setTranslateY(10);
+        rightEye.setFill(Color.RED);
 
         Circle mouth = new Circle(0, 0, 7);
         mouth.setTranslateY(-5);
         mouth.setFill(Color.BLACK);
 
-        StackPane ghostLayout = new StackPane(ghost, leftEye, rightEye, mouth);
+        StackPane ghostLayout = new StackPane(lice, leftEye, rightEye, mouth);
 //        ghostLayout.getChildren().add(ghost);
         ghostLayout.setVisible(false);
 
@@ -73,7 +73,7 @@ public class ChambersOfDeath implements AppScene{
             goBack.handle(e);
         });
 
-        Button revealButton = new Button("Reveal Secret");
+        Button revealButton = new Button("Check out the bed");
         revealButton.setOnAction(e -> {
             if (answer.equalsIgnoreCase(input.getText().trim())) {
                 secret.setText(secretText);
@@ -81,7 +81,7 @@ public class ChambersOfDeath implements AppScene{
                 secret.setStyle(String.format("%s-fx-text-fill: %s;", secret.getStyle(), red));
                 ghostLayout.setVisible(true);
             } else {
-                secret.setText("Nothing happens...");
+                secret.setText("I travel by crawling, not flying or hopping. \n I lay eggs on a person's head, but I can also live in their clothes. \n What am I?");
                 secret.setVisible(true);
                 secret.setStyle(String.format("%s-fx-text-fill: %s;", secret.getStyle(), "black"));
             }
