5c1573f2db46f356215e285a0f043445521b0e14
ninabarnes <ninabarnes@hunschool.org>
2025-10-20 11:29:03 -0400

    Refactored final dinner with methods that are more concise.

--- FX/HauntedMansion/src/TheFinalDinner.java
+++ FX/HauntedMansion/src/TheFinalDinner.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -13,111 +14,138 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class TheFinalDinner extends Application implements AppScene{
+    public Text makeText(String message, int wrapping, String font, int size, String style, int xLayout, int yLayout,
+                         boolean isVisible){
+        Text newText = new Text(message);
+        newText.setWrappingWidth(wrapping);
+        newText.setFont(Font.font(font, size));
+        newText.setStyle(style);
+        newText.setLayoutX(xLayout);
+        newText.setLayoutY(yLayout);
+        newText.setVisible(isVisible);
+        return newText;
+    }
+
+    public Rectangle makeRectangle(int xPos, int yPos, int width, int height,
+                                   Color fillColor, Color borderColor, int strokeWidth, boolean isVisible){
+        Rectangle newRectangle = new Rectangle(xPos, yPos, width, height);
+        newRectangle.setFill(fillColor);
+        newRectangle.setStroke(borderColor);
+        newRectangle.setStrokeWidth(strokeWidth);
+        newRectangle.setVisible(isVisible);
+        return newRectangle;
+    }
+
+    public Rectangle makeRectangle(int xPos, int yPos, int width, int height, int hue, double saturation,
+                                   double brightness, String style, boolean isVisible){
+        Rectangle bgRectangle = new Rectangle(xPos, yPos, width, height);
+        bgRectangle.setFill(Color.hsb(hue,saturation,brightness));
+        bgRectangle.setStyle(style);
+        bgRectangle.setVisible(isVisible);
+        return bgRectangle;
+    }
+
+    public Label makeLabel(String name, String font, int size, String style, int xLayout, boolean isVisible){
+        Label newLabel = new Label(name);
+        newLabel.setFont(Font.font(font,size));
+        newLabel.setStyle(style); //nts fix border color
+        newLabel.setLayoutX(xLayout);
+        newLabel.setVisible(isVisible);
+        return newLabel;
+    }
+
+    public Button makeButton(String text, String style, int xLayout, int yLayout, boolean isVisible){
+        Button newButton = new Button(text);
+        newButton.setStyle(style);
+        newButton.setLayoutX(xLayout);
+        newButton.setLayoutY(yLayout);
+        newButton.setVisible(isVisible);
+        return newButton;
+    }
+
+    public static EventHandler<ActionEvent> pressButton (Text firstText, Text secondText, Button buttonOne, Button buttonTwo){
+        return e -> {
+            firstText.setVisible(false);
+            secondText.setVisible(true);
+            buttonOne.setVisible(false);
+            buttonTwo.setVisible(true);
+        };
+    }
+    public static void pressButton(Button mainButton, Text txtOne, Text txtTwo, Text txtThree, Button btnOne, Button btnTwo){
+        mainButton.setOnAction(e -> {
+            txtOne.setVisible(false);
+            txtTwo.setVisible(false);
+            btnOne.setVisible(false);
+            btnTwo.setVisible(false);
+            txtThree.setVisible(true);
+        });
+    }
+    public static void pressButton(Button mainButton, Text txtOne, Text txtTwo, Button btnOne, Button btnTwo){
+        mainButton.setOnAction(e -> {
+            txtOne.setVisible(false);
+            txtTwo.setVisible(true);
+            btnOne.setVisible(false);
+            btnTwo.setVisible(true);
+        });
+    }
+
+    public static void setGroupLayout(Node node, Rectangle rect1, int xPos1, Rectangle rect2, int xPos2,
+                                      Rectangle rect3, int xPos3, int yLayout){
+        rect1.setLayoutX(xPos1);
+        rect2.setLayoutX(xPos2);
+        rect3.setLayoutX(xPos3);
+        node.setLayoutY(yLayout);
+    }
+
+    String introParagraph = "The air feels stale. Between the spotlights of the moon, you can " +
+            "see spiders crawling across the walls. You sit down to take a meal, and there is already " +
+            "a platter in front of you.";
+
+    String eatButtonStyle = "-fx-background-color:#c49e9eff;-fx-text-fill: #682020;-fx-font-size: 26px;" +
+            " -fx-border radius: 8px;-fx-font-weight: bold;-fx-border-color:#460e0e; -fx-font-family:Georgia;";
+
+    String openNoteBtnStyle = "-fx-background-color:#cec7c7ff;-fx-text-fill:#291313;" +
+            "-fx-font-size: 26px; " + "-fx-border radius: 8px;-fx-font-family:Georgia;";
+
+    String homeBtnStyle = "-fx-background-color:#000000ff;-fx-text-fill:#ffffff;" +
+            "-fx-font-size: 18px; " + "-fx-border radius: 8px;-fx-font-family:Georgia;";
+
+
     @Override
     public Scene getScene(EventHandler<ActionEvent> goBack) {
-        Label titleText = new Label("The Final Dinner");
-        titleText.setFont(Font.font("Georgia",30));
-        titleText.setStyle("-fx-font-weight:bold; -fx-text-fill: #FFFFFF;" +
-                "-fx-background-color:#383288ff;"); //nts fix border color
-        titleText.setLayoutX(725);
-
-        Rectangle txtRectangle = new Rectangle(10, 10, 600, 300);
-        txtRectangle.setFill(Color.hsb(244,0.727,0.173));
-        txtRectangle.setStyle("-fx-stroke: #4D1005; -fx-stroke-width: 1");
-
-        Text spookyTxt = new Text("The air feels stale. Between the spotlights of the moon, you can " +
-                "see spiders crawling across the walls. You sit down to take a meal, and there is already " +
-                "a platter in front of you.");
-        spookyTxt.setWrappingWidth(550);
-        spookyTxt.setFont(Font.font("Georgia", 27));
-        spookyTxt.setStyle("-fx-fill: #faf7ff");
-        spookyTxt.setLayoutX(17);
-        spookyTxt.setLayoutY(37);
-
+        Label titleText = makeLabel("The Final Dinner", "Georgia", 30,
+                "-fx-font-weight:bold; -fx-text-fill: #FFFFFF; -fx-background-color:#383288ff;",
+                725, true);
+        Rectangle txtRectangle = makeRectangle(10,10,600,300,244,
+                0.727,0.173, "-fx-stroke: #4D1005; -fx-stroke-width: 1", true);
+        Text spookyTxt = makeText(introParagraph, 550, "Georgia", 27,
+                "-fx-fill: #faf7ff", 17, 37, true);
         Pane bgText = new Pane(txtRectangle, spookyTxt);
 
-        Rectangle tableHead = new Rectangle(-670,360,400,20);
-        tableHead.setFill(Color.SIENNA);
-        tableHead.setStroke(Color.SADDLEBROWN);
-        tableHead.setStrokeWidth(2);
-
-        Rectangle tableLeg1 = new Rectangle(-680,380, 16,200);
-        tableLeg1.setFill(Color.SIENNA);
-        tableLeg1.setStroke(Color.SADDLEBROWN);
-        tableLeg1.setStrokeWidth(2);
-
-        Rectangle tableLeg2 = new Rectangle(-353, 380, 16, 200);
-        tableLeg2.setFill(Color.SIENNA);
-        tableLeg2.setStroke(Color.SADDLEBROWN);
-        tableLeg2.setStrokeWidth(2);
-
+        Rectangle tableHead = makeRectangle(-670, 360, 400, 28, Color.SIENNA,
+                Color.SADDLEBROWN, 2, true);
+        Rectangle tableLeg1 = makeRectangle(-680, 387, 16, 200, Color.SIENNA,
+                Color.SADDLEBROWN, 2, true);
+        Rectangle tableLeg2 = makeRectangle(-353, 387, 16, 200, Color.SIENNA,
+                Color.SADDLEBROWN, 2, true);
         Pane table = new Pane(tableHead,tableLeg1,tableLeg2);
-        tableHead.setLayoutX(0);
-        tableHead.setLayoutX(700);
-        tableLeg1.setLayoutX(50);
-        tableLeg1.setLayoutX(740);
-        tableLeg2.setLayoutX(160);
-        tableLeg2.setLayoutX(740);
-        table.setLayoutY(175);
+        setGroupLayout(table,tableHead,700,tableLeg1,740,tableLeg2,740,175);
 
-        Button eatBtn = new Button("Enjoy your meal");
-        eatBtn.setStyle("-fx-background-color: #c49e9eff;-fx-text-fill: #682020; " +
-                "-fx-font-size: 26px; -fx-border radius: 8px;-fx-font-weight: bold;" +
-                "-fx-border-color:#460e0e; -fx-font-family:Georgia;");
-        eatBtn.setLayoutX(20);
-        eatBtn.setLayoutY(325);
+        Text headTxt = makeText("You lift the serving dish to find a severed head and" +
+                " something engraved in the plate", 550, "Georgia", 27, "-fx-fill: #faf7ff",
+                17, 37, false);
+        Text noteTxt = makeText("Don't trust the voices.", 550, "monospace", 27,
+                "-fx-fill: #b61d1d", 17, 37, false);
+
+        Button homeBtn = makeButton("Home", homeBtnStyle, 915, 650, true);
+        Button openNote = makeButton("Read the message", openNoteBtnStyle, 20, 325, false);
+        Button eatBtn = makeButton("Enjoy your meal", eatButtonStyle, 20, 325, true);
         Pane eat = new Pane(eatBtn);
-
-
-        Button openNote = new Button("Read the message");
-        openNote.setStyle("-fx-background-color:#cec7c7ff;-fx-text-fill:#291313;" +
-                "-fx-font-size: 26px; " + "-fx-border radius: 8px;-fx-font-family:Georgia;");
-        openNote.setLayoutX(20);
-        openNote.setLayoutY(325);
-        openNote.setVisible(false);
-
-        Text headTxt = new Text("You lift the serving dish to find a severed head and" +
-                " something engraved in the plate");
-        headTxt.setWrappingWidth(550);
-        headTxt.setFont(Font.font("Georgia", 27));
-        headTxt.setStyle("-fx-fill: #faf7ff");
-        headTxt.setLayoutX(17);
-        headTxt.setLayoutY(37);
-        headTxt.setVisible(false);
-
-        Text noteTxt = new Text("Don't trust the voices.");
-        noteTxt.setWrappingWidth(550);
-        noteTxt.setFont(Font.font("Georgia", 27));
-        noteTxt.setStyle("-fx-fill: #b61d1d");
-        noteTxt.setLayoutX(17);
-        noteTxt.setLayoutY(37);
-        noteTxt.setVisible(false);
-
-        Button homeBtn = new Button("Home");
-        homeBtn.setStyle("-fx-background-color:#000000ff;-fx-text-fill:#ffffff;" +
-                "-fx-font-size: 18px; " + "-fx-border radius: 8px;-fx-font-family:Georgia;");
-        homeBtn.setLayoutX(915);
-        homeBtn.setLayoutY(650);
-        homeBtn.setVisible(true);
-
-        eatBtn.setOnAction(e -> {
-            spookyTxt.setVisible(false);
-            headTxt.setVisible(true);
-            eatBtn.setVisible(false);
-            openNote.setVisible(true);
-        });
-
-        openNote.setOnAction(e -> {
-            spookyTxt.setVisible(false);
-            headTxt.setVisible(false);
-            eatBtn.setVisible(false);
-            openNote.setVisible(false);
-            noteTxt.setVisible(true);
-        });
+        pressButton(openNote,spookyTxt,headTxt,noteTxt,eatBtn,openNote);
+        pressButton(eatBtn,spookyTxt,headTxt,eatBtn,openNote);
 
         Pane layout = new Pane(titleText,bgText, table, eat, openNote, headTxt,noteTxt,homeBtn);
         layout.setBackground(Background.fill(Color.hsb(244,0.632,0.535)));
-
         Scene displayScene = new Scene(layout, 1000, 700);
         return displayScene;
     }
