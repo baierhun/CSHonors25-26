99520f042a4d28847830d489713fcc1f54d503b9
Mr. Baier <paulbaier@hunschool.org>
2025-10-29 11:34:27 -0400

    goHome with SceneSetter

--- FX/HauntedMansion/src/SceneSetter.java
+++ FX/HauntedMansion/src/SceneSetter.java
@@ -3,11 +3,18 @@ import javafx.stage.Stage;
 
 public class SceneSetter {
     private Stage stage;
-    public SceneSetter(Stage stage) {
+    private Scene home;
+
+    public SceneSetter(Stage stage, Scene home) {
         this.stage = stage;
+        this.home = home;
     }
 
     public void goTo(Scene scene) {
         stage.setScene(scene);
+    }
+
+    public void goHome() {
+        stage.setScene(home);
     }
 }
