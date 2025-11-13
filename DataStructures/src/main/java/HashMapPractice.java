6e18b1905c4ee006aa6831b59a1ef5ae972441c3
Mr. Baier <paulbaier@hunschool.org>
2025-11-13 09:04:08 -0500

    finish hashmap practice

--- DataStructures/src/main/java/HashMapPractice.java
+++ DataStructures/src/main/java/HashMapPractice.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class HashMapPractice {
     public static void main(String[] args) {
@@ -7,5 +8,14 @@ public class HashMapPractice {
         System.out.println(grades.get("cindy"));
         grades.put("cindy", 80);
         System.out.println(grades.get("cindy"));
+        grades.isEmpty();
+        grades.containsKey("cindy");
+        grades.putIfAbsent("cindy", 90);
+        grades.put("bob", 20);
+
+        for(Map.Entry<String, Integer> item : grades.entrySet()) {
+            System.out.println("%s: %d".formatted(item.getKey(), item.getValue()));
+        }
+
     }
 }
