c4c9636135aae16ca3a6c055de73342b9fb819a3
Mr. Baier <paulbaier@hunschool.org>
2025-12-05 20:35:01 -0500

    AoC: day 2-1 oo

--- AoC/src/main/java/Day2.java
+++ AoC/src/main/java/Day2.java
@@ -1,38 +1,57 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.function.Function;
 
 public class Day2 {
-    public static long sum = 0;
+    private long sum = 0;
+    private Function<String, Boolean> isInvalid;
 
-    private static boolean isInvalid(String number) {
+    public Day2(Function<String, Boolean> isInvalid) {
+        this.isInvalid = isInvalid;
+    }
+
+    public static boolean isInvalidOne(String number) {
+        if (number.length() % 2 == 1) return false;
         int middle = number.length() / 2;
         String left = number.substring(0, middle);
         String right = number.substring(middle);
         return left.equals(right);
     }
 
-    private static void checkRange(String start, String end) {
+    private boolean isInvalid(String number) {
+        return isInvalid.apply(number);
+    }
+
+    private void checkRange(String start, String end) {
         long startL = Long.parseLong(start);
         long endL = Long.parseLong(end);
         for (long s = startL; s <= endL; s++) {
             String current = String.valueOf(s);
-            if (current.length() % 2 == 1) continue;
             if (isInvalid(current)) {
-                Day2.sum += s;
+                sum += s;
             }
         }
     }
 
     // range is ###-###
-    private static void processRange(String range) {
+    private void processRange(String range) {
         String[] splitRange = range.split("-");
         String startRange = splitRange[0];
         String endRange = splitRange[1];
         checkRange(startRange, endRange);
     }
 
-    private static void attempt2(Scanner in) {
+    private void runOne(Scanner in) {
+        String allData = in.nextLine();
+        String[] ranges = allData.split(",");
+        for (String range : ranges) {
+            processRange(range);
+        }
+    }
+
+    //  ------------------------------------------------------------------------------------------------
+    private void partTwo(Scanner in) {
         String allData = in.nextLine();
         String[] ranges = allData.split(",");
         for (String range : ranges) {
@@ -43,8 +62,8 @@ public class Day2 {
     public static void main(String[] args) throws FileNotFoundException {
         File data = new File("AoC/src/main/java/day2-1");
         Scanner in = new Scanner(data);
-        attempt2(in);
-        System.out.println(Day2.sum);
-
+        Day2 app = new Day2(Day2::isInvalidOne);
+        app.runOne(in);
+        System.out.println(app.sum);
     }
 }
