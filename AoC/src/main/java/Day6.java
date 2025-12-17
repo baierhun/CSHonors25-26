ea91531c1a36721d1fc1b49323ef810169a5e0de
Mr. Baier <paulbaier@hunschool.org>
2025-12-17 06:17:16 -0500

    AoC: day 6-2

--- AoC/src/main/java/Day6.java
+++ AoC/src/main/java/Day6.java
@@ -1,7 +1,115 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
+
+class Equations implements Iterable<Equation> {
+    ArrayList<Equation> equations = new ArrayList<>();
+    int index = 0;
+
+    public void add(Equation eq) {
+        equations.add(eq);
+    }
+
+    public Equation get(int index) {
+        return equations.get(index);
+    }
+
+    public List<Equation> subList(int start, int end) {
+        return equations.subList(start, end);
+    }
+
+    public int size() {
+        return equations.size();
+    }
+
+    public void split(String opsLine) {
+        char[] opsChars = opsLine.toCharArray();
+        String op = String.valueOf(opsChars[0]);
+        char current;
+        for (int i = 1; i < opsChars.length; i++) {
+            current = opsChars[i];
+            if (current == '*' || current == '+') {
+                this.add(new Equation(op));
+                op = String.valueOf(current);
+            } else {
+                op += " ";
+            }
+        }
+        this.add(new Equation(op));
+    }
+
+    public long sum() {
+        long total = 0L;
+        for (Equation eq : equations) {
+            total += eq.total();
+        }
+        return total;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new Iterator() {
+            @Override
+            public boolean hasNext() {
+                return index < equations.size();
+            }
+
+            @Override
+            public Equation next() {
+                return equations.get(index++);
+            }
+        };
+    }
+}
+
+class Equation {
+    ArrayList<String> numberLines = new ArrayList<>();
+    BiFunction<Long, Long, Long> op;
+    int spaces;
+    Long total;
+
+    public Equation(String opLine) {
+        this.addOp(opLine);
+    }
+
+    public void addLine(String line) {
+        numberLines.add(line.replaceAll(" ", "0"));
+    }
+
+    private void countSpaces(String s) {
+        spaces = 0;
+        for (char a : s.toCharArray()) {
+            if (a == ' ') spaces++;
+        }
+    }
+
+    public void addOp(String op) {
+        countSpaces(op);
+        if (op.contains("+")) {
+            this.op = (Long x, Long y) -> Math.addExact(x, y);
+            total = 0L;
+        } else {
+            this.op = (Long x, Long y) -> Math.multiplyExact(x, y);
+            total = 1L;
+        }
+    }
+
+    public Long total() {
+        int digits = spaces;
+        for (int i = 0; i < digits; i++) {
+            String verticalNumber = "";
+            for (String line : numberLines) {
+                String digit = line.substring(i, i + 1);
+                if (digit.equals("0")) continue;
+                verticalNumber += line.substring(i, i + 1);
+            }
+            total = op.apply(total, Long.parseLong(verticalNumber));
+        }
+        return total;
+    }
+}
 
 public class Day6 {
     File fileData = new File("AoC/src/main/java/day6-1");
@@ -51,8 +159,39 @@ public class Day6 {
         addLines(ops);
     }
 
+    public void runPartII() {
+        while (in.hasNext()) {
+            data.push(in.nextLine());
+        }
+        String opsLine = data.pop();
+        ArrayList<String> dataLines = new ArrayList<>();
+        while (!data.empty()) dataLines.add(data.pop());
+        Equations eqs = new Equations();
+        eqs.split(opsLine);
+        int start = 0;
+        int end;
+        String line;
+        for (Equation eq : eqs.subList(0, eqs.size() - 1)) {
+            end = start + eq.spaces;
+            for (int i = dataLines.size() - 1; i >= 0; i--) {
+                line = dataLines.get(i);
+                eq.addLine(line.substring(start, end).replaceAll(" ", "0"));
+            }
+            start = end + 1;
+        }
+        // last one
+        Equation last = eqs.get(eqs.size() - 1);
+        last.spaces += 1;
+        for (int i = dataLines.size() - 1; i >= 0; i--) {
+            line = dataLines.get(i);
+            last.addLine(line.substring(start).replaceAll(" ", "0"));
+        }
+
+        System.out.println(eqs.sum());
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
         Day6 app = new Day6();
-        app.run();
+        app.runPartII();
     }
 }
